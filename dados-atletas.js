c46ee9d2700c2aa5ef6b577c89e2748c6c2727c1
Lucas Boschet Moraes <lucasmoraes86@hotmail.com>
2024-08-21 17:58:51 -0300

    first commit

---
+++ dados-atletas.js
@@ -0,0 +1,44 @@
+class Atleta {
+
+    constructor(nome, idade, peso, altura, notas) {
+        console.log(nome, idade, peso, altura, notas)
+    }
+
+    calculaCategoria() {
+
+    }
+
+    calculaIMC() {
+
+    }
+
+    calculaMediaValida() {
+
+    }
+
+    obtemNomeAtleta() {
+
+    }
+
+    obtemIdadeAtleta() {
+
+    }
+
+    obtemPesoAtleta() {
+
+    }
+
+    obtemNotasAtleta() {
+
+    }
+
+    obtemCategoria() {
+
+    }
+
+    obtemMediaValida() {
+
+    }
+}
+
+const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88]);
